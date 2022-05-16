a089c81e66eeda297ede207ee26009ff2c427a61
Aryeetey <saaryeetey.cos20@gmail.com>
2022-05-16 08:37:20 +0000

    Add Implementation of Stack with Array List

--- src/main/kotlin/stacks/Stack.kt
+++ src/main/kotlin/stacks/Stack.kt
@@ -52,6 +52,35 @@ class Stack<T> {
     }
 }
 
+/**
+ * Stack which uses Arrays
+ */
+class Stack2<T> {
+    private val items = ArrayList<T>()
+
+    fun push(item: T) {
+        items.add(item)
+    }
+
+    fun pop(): T? {
+        if (items.size < 1) return null
+        val lastPos = items.size - 1
+        val lastItem = items[lastPos]
+        items.removeAt(lastPos)
+        return lastItem
+    }
+
+    fun peek(): T? {
+        if (items.size < 1) return null
+        val lastPos = items.size - 1
+        return items[lastPos]
+    }
+
+    fun hasMore(): Boolean {
+        return (items.size >= 1)
+    }
+}
+
 class Node<T>(val value: T, var next: Node<T>? = null)
 
 fun main() {
@@ -64,5 +93,17 @@ fun main() {
 
     while (browserHistory.hasMore()) {
         println(browserHistory.pop())
+    }
+
+    println("=======================")
+    val browserHistory2 = Stack2<String>()
+    browserHistory2.apply {
+        push("A.com")
+        push("B.com")
+        push("C.com")
+    }
+
+    while (browserHistory2.hasMore()) {
+        println(browserHistory2.pop())
     }
 }
